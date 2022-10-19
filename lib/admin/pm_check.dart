022b0547295c8d7f0217f4b9ba51e62492a19a42
Jonathan Ma <jonathanmajh@gmail.com>
2022-10-19 10:54:19 -0400

    remove dup widgets

--- lib/admin/pm_check.dart
+++ lib/admin/pm_check.dart
@@ -144,74 +144,10 @@ class _PmCheckPageState extends State<PmCheckPage> {
                     width: 550,
                     child: Consumer<TemplateNotifier>(
                         builder: (context, value, child) {
-                      return Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: [
-                                ElevatedButton(
-                                  style: ButtonStyle(
-                                      shape: MaterialStateProperty.all<
-                                              RoundedRectangleBorder>(
-                                          const RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.only(
-                                      topLeft: Radius.circular(18),
-                                      bottomLeft: Radius.circular(18),
-                                    ),
-                                  ))),
-                                  onPressed: () {
-                                    pickTemplates(context);
-                                  },
-                                  child: Row(
-                                    children: const [
-                                      Icon(Icons.file_open),
-                                      Text(' Open Templates'),
-                                    ],
-                                  ),
-                                ),
-                                ElevatedButton(
-                                  style: ButtonStyle(
-                                      shape: MaterialStateProperty.all<
-                                              RoundedRectangleBorder>(
-                                          const RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.only(
-                                      topRight: Radius.circular(18),
-                                      bottomRight: Radius.circular(18),
-                                    ),
-                                  ))),
-                                  onPressed: () {
-                                    context
-                                        .read<TemplateNotifier>()
-                                        .clearTemplates();
-                                    var box = Hive.box('jpNumber');
-                                    box.clear();
-                                    box = Hive.box('pmNumber');
-                                    box.clear();
-                                    box = Hive.box('routeNumber');
-                                    box.clear();
-                                  },
-                                  child: Row(
-                                    children: const [
-                                      Icon(Icons.delete_sweep),
-                                      Text(' Clear Templates'),
-                                    ],
-                                  ),
-                                ),
-                              ]),
-                          const Divider(
-                            height: 15,
-                            thickness: 1,
-                            indent: 10,
-                            endIndent: 10,
-                            color: Color.fromARGB(255, 255, 255, 255),
-                          ),
-                          Expanded(
-                              child: ListView(
-                            children: buildPMList(value),
-                          ))
-                        ],
-                      );
+                      return Expanded(
+                          child: ListView(
+                        children: buildPMList(value),
+                      ));
                     })),
                 const VerticalDivider(
                   width: 20,
