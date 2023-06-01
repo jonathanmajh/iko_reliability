a667c0b028f122ece3d6c1165418e7d1a00637ee
Desumai <desumai@github.com>
2023-06-01 08:45:02 -0400

    User input locking
    Implemented dialog window that appears when application is waiting on a process.
    Currently only works with loading assets from end drawer

--- lib/admin/process_state_notifier.dart
+++ lib/admin/process_state_notifier.dart
@@ -1,6 +1,8 @@
 import 'dart:core';
 
 import 'package:flutter/material.dart';
+import 'package:iko_reliability_flutter/settings/theme_manager.dart';
+import 'package:provider/provider.dart';
 
 ///records the application processing states
 class ProcessStateNotifier extends ChangeNotifier {
@@ -15,6 +17,8 @@ class ProcessStateNotifier extends ChangeNotifier {
 
       largestState =
           3; //the largest state int. Update this when creating new state int consts. (Could use dart:mirrors instead)
+
+  bool alertDialogShowing = false;
 
   Map<int, bool> processStates = <int, bool>{};
 
@@ -36,8 +40,28 @@ class ProcessStateNotifier extends ChangeNotifier {
 
   ///sets the process specified by [process] to [value]. Checks if any processes are running and returns that bool if [checkProcesses] is [true]. Notifies all listeners if [notifyListeners] is [true].
   bool setProcessState(int process, bool value,
-      {bool checkProcesses = false, bool notifyListeners = true}) {
+      {bool checkProcesses = false,
+      bool notifyListeners = true,
+      bool createDialog = false,
+      bool closeDialog = false,
+      BuildContext? context}) {
     if (processStates.containsKey(process)) {
+      if (createDialog && value) {
+        if (context != null) {
+          processingDialog(context);
+        } else {
+          throw Exception(
+              'If [createDialog] is true, then [context] must be given');
+        }
+      }
+      if (closeDialog && !value) {
+        if (context != null) {
+          popProcessingDialog(context);
+        } else {
+          throw Exception(
+              'If [closeDialog] is true, the [context] must be given');
+        }
+      }
       processStates[process] = value;
       if (notifyListeners) {
         super.notifyListeners();
@@ -52,5 +76,36 @@ class ProcessStateNotifier extends ChangeNotifier {
   bool absorbInput() {
     //TODO: if certain processes are running, should return [true]
     return false;
+  }
+
+  Future<void> processingDialog(BuildContext context) {
+    alertDialogShowing = true;
+    return showDialog<void>(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Loading...'),
+            content: CircularProgressIndicator(
+              color: Colors.red, //Provider.of<ThemeData>(context).primaryColor,
+            ),
+            actions: [
+              TextButton(
+                child: const Text('Cancel'),
+                onPressed: () {
+                  //TODO: cancel current process
+                  popProcessingDialog(context);
+                },
+              )
+            ],
+          );
+        },
+        barrierDismissible: false);
+  }
+
+  void popProcessingDialog(context) {
+    if (alertDialogShowing) {
+      Navigator.pop(context);
+      alertDialogShowing = false;
+    }
   }
 }
