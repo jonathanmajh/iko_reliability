35d856daff31f169339549be4afaaaa93c46bf14
Jonathan Ma <jonathanmajh@gmail.com>
2023-08-24 16:29:20 -0400

    another one

--- lib/criticality/criticality_notifier.dart
+++ lib/criticality/criticality_notifier.dart
@@ -14,31 +14,3 @@ class SystemsNotifier extends ChangeNotifier {
     notifyListeners();
   }
 }
-
-class RpnCriticalityNotifier extends ChangeNotifier {
-  int targetVL = 30;
-  int targetL = 25;
-  int targetM = 20;
-  int targetH = 15;
-
-  int lowLowerLimit = 15;
-  int mediumLowerLimit = 100;
-  int highLowerLimit = 500;
-  int veryHighLowerLimit = 1000;
-
-  int getCriticality(double rpm) {
-    if (rpm < lowLowerLimit) {
-      return 1;
-    }
-    if (rpm < mediumLowerLimit) {
-      return 3;
-    }
-    if (rpm < highLowerLimit) {
-      return 5;
-    }
-    if (rpm < veryHighLowerLimit) {
-      return 7;
-    }
-    return 9;
-  }
-}
