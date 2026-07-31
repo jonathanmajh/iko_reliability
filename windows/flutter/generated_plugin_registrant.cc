0f63bba391d72f187e50a8c77d9c734c1cdaa2f9
Jonathan Ma <jonathanmajh@gmail.com>
2026-07-31 14:40:22 -0400

    update deps

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -9,7 +9,6 @@
 #include <file_selector_windows/file_selector_windows.h>
 #include <flutter_window_close/flutter_window_close_plugin.h>
 #include <irondash_engine_context/irondash_engine_context_plugin_c_api.h>
-#include <sqlite3_flutter_libs/sqlite3_flutter_libs_plugin.h>
 #include <super_native_extensions/super_native_extensions_plugin_c_api.h>
 #include <url_launcher_windows/url_launcher_windows.h>
 #include <window_size/window_size_plugin.h>
@@ -21,8 +20,6 @@ void RegisterPlugins(flutter::PluginRegistry* registry) {
       registry->GetRegistrarForPlugin("FlutterWindowClosePlugin"));
   IrondashEngineContextPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("IrondashEngineContextPluginCApi"));
-  Sqlite3FlutterLibsPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("Sqlite3FlutterLibsPlugin"));
   SuperNativeExtensionsPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("SuperNativeExtensionsPluginCApi"));
   UrlLauncherWindowsRegisterWithRegistrar(
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -6,13 +6,13 @@ list(APPEND FLUTTER_PLUGIN_LIST
   file_selector_windows
   flutter_window_close
   irondash_engine_context
-  sqlite3_flutter_libs
   super_native_extensions
   url_launcher_windows
   window_size
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
+  jni
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
